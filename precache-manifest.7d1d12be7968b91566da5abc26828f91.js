db7e8c239e55253cd1af912bc8180fdc19b4c3c4
NBobrova <nastyabobr@yandex.ru>
2020-03-16 17:35:52 +0300

    Updates

---
+++ precache-manifest.7d1d12be7968b91566da5abc26828f91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6c8b06d3e08e4e203a78cc47f5f946",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "97438e76bbd049e1a665",
+    "url": "/test/static/css/main.8a368dd4.chunk.css"
+  },
+  {
+    "revision": "e09afb70e07096d11f99",
+    "url": "/test/static/js/2.7c958567.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/test/static/js/2.7c958567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97438e76bbd049e1a665",
+    "url": "/test/static/js/main.528979b5.chunk.js"
+  },
+  {
+    "revision": "6bbc3f51202c28fa0a7b",
+    "url": "/test/static/js/runtime-main.5b4329d1.js"
+  },
+  {
+    "revision": "b38f9c1fc6d7a868e0f63263390a7283",
+    "url": "/test/static/media/pen.b38f9c1f.png"
+  }
+]);
